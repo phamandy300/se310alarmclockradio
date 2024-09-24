907bae668658f8cf447a17f5d645fca9a1d8d79b
Andy <ap3879@drexel.edu>
2024-09-24 00:55:22 -0400

    added clock methods and alarm constructor

--- src/Alarm.java
+++ src/Alarm.java
@@ -1,2 +1,8 @@
 public class Alarm extends Clock{
+
+    public Alarm(int hours, int minutes, String period) {
+        super(hours, minutes, period);
+    }
+
+
 }
--- src/Clock.java
+++ src/Clock.java
@@ -1,14 +1,47 @@
 public class Clock {
-    public int hour;
-    public int minute;
+    public int hours;
+    public int minutes;
+    private int seconds;
     public String period;
 
-    public Clock(int hour, int minute, String period) {
-        this.hour = hour;
-        this.minute = minute;
+    public Clock(int hours, int minutes, String period) {
+        this.hours = hours;
+        this.minutes = minutes;
         this.period = period.toUpperCase();
     }
 
-    // return or display formatted time
-    //
+    public void setTime(int setHour, int setMinute) {
+        hours = setHour;
+        minutes = setMinute;
+    }
+
+    public void tick() {
+        seconds++;
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+        }
+
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+
+        if (hours >= 60) {
+            hours = 0;
+            if (period.equals("AM")) {
+                period = "PM";
+            } else {
+                period = "AM";
+            }
+        }
+
+    }
+
+    public String display() {
+        return period;
+    }
+
 }
+
+
