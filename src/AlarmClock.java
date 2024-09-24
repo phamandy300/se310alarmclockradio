135daae5e07258fad347f7d23cdf92e99874a455
Andy <ap3879@drexel.edu>
2024-09-24 01:03:06 -0400

    AlarmClock vars

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -1,4 +1,6 @@
 public class AlarmClock extends Clock{
+    public int alarmHR;
+    public int alarmMIN;
 
     public AlarmClock(int hours, int minutes, String period) {
         super(hours, minutes, period);
