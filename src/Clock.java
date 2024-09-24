ec42907a030a7f7e93480689a38f411ef3f25632
Andy <ap3879@drexel.edu>
2024-09-24 04:55:59 -0400

    fixed Clock

--- src/Clock.java
+++ src/Clock.java
@@ -16,24 +16,27 @@ public class Clock {
     }
 
     public void tick() {
-        seconds++;
+        ++seconds;
         if (seconds >= 60) {
             seconds = 0;
-            minutes++;
+            ++minutes;
         }
 
         if (minutes >= 60) {
             minutes = 0;
-            hours++;
+            ++hours;
         }
 
-        if (hours > 12) {
-            hours = 0;
+        if (hours == 12) {
             if (period.equals("AM")) {
                 period = "PM";
             } else {
                 period = "AM";
             }
+        }
+
+        if (hours > 12) {
+            hours = 1;
         }
 
     }
